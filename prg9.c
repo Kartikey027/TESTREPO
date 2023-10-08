b9486fe53c648455fd687ddec0427f41f8dd1f20
kartikey027 <saxenakartikey208@gmail.com>
2023-10-08 20:49:02 +0530

    to display a pattern

---
+++ prg9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,k,sp;
+    sp=4;
+    for(i=1;i<=5;i++)
+    {
+        for(k=1;k<=sp;k++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+
+        }
+        printf("\n");
+        sp--;
+    }
+    return 0;
+}

c917dcf787a9f2ab0c88de361a8789473433038a
kartikey027 <saxenakartikey208@gmail.com>
2023-10-16 14:54:11 +0530

    to take transpose of an array

---
+++ prg11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,k,l,sp;
+    sp=8;
+    for(i=1;i<=5;i++)
+    {
+       for(j=1;j<=i;j++)
+       {
+        printf("%d",j);
+       }
+       for(k=1;k<=sp;k++)
+       {
+        printf(" ");
+       }
+       sp-=2;
+       for(l=i;l>=1;l--)
+       {
+        printf("%d",l);
+       }
+       printf("\n");
+    }
+    return (0);
+}
